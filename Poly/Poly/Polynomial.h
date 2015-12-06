424ebcf1cb2fde8b0218d1b9445341d557677abb
Melak <melakf2@gmail.com>
2015-12-06 16:22:15 +0200

    edit class Polynomial

--- Poly/Poly/Polynomial.h
+++ Poly/Poly/Polynomial.h
@@ -1,47 +1,49 @@
-#pragma once
-<<<<<<< HEAD
-#pragma once
-=======
->>>>>>> db4acc44e24f790379d122bc86d264be4ee195b9
 #ifndef POLYNOMIAL_H
 #define POYNOMIAL_H
 #include <iostream>
 using namespace std;
 class Polynomial
-{   
-	
-	//non member function
-	
+{   	
+    //non member function
 	friend ostream & operator<<(ostream & output,const Polynomial &polynomial);
 	friend istream & operator>>(istream  & input, Polynomial &polynomial);
 	//non-member function, get two arguments, unlike non-static member function that get one argument.
-	friend Polynomial &operator+(Polynomial &,Polynomial&);
-    friend Polynomial &operator-(Polynomial &,Polynomial&);
-    friend Polynomial & operator*(const Polynomial &,const Polynomial &);
-	friend Polynomial &operator*(int , Polynomial & );
-
-protected:
+	
+	//friend Polynomial &operator+(const double ,Polynomial&);
+	friend Polynomial &operator-(const double,Polynomial&);
+	//friend Polynomial &operator+(Polynomial & p1, const Polynomial & p2);
+	friend Polynomial &operator*(Polynomial &, const double);//p1*k
+	friend Polynomial &operator*(const double , Polynomial&);//k*p
+private:
 	//data member
 	double * array_coefficient;
 	int size;
 public:
 	//member function
-	Polynomial(int=1); // ctor with degree zero & arr size is 1
+	Polynomial();
+	Polynomial(int); // ctor with degree zero & arr size is 1
 	~Polynomial(); // dctor
 	Polynomial( const Polynomial &); //copy ctor
 	void set_size_of_array(int); //setter 
-	int get_size_of_array()const; //getter	
+	int get_size_of_array()const; //getter
+
+
+	//opertor-overloading
 	bool operator== (const Polynomial &) const;
 	bool operator!= (const Polynomial & right) const { return !(*this == right);};
 	bool operator> (const Polynomial &) const;
 	bool operator< (const Polynomial & right) const{ return !(*this > right);};
 	const Polynomial & operator+= (const Polynomial &); //operator+=
-	const Polynomial & operator-= (const Polynomial & )const; //operator-=
-	const Polynomial & operator= (const Polynomial &); //assignment (array) operator   
-
-	int & operator[] (int);
-	const int & operator[] (int)const;
+	const Polynomial & operator-= (const Polynomial & ); // p1-=p2
+	const Polynomial &operator*=(const Polynomial & );//p1=p3*p2
+	const Polynomial & operator= (const Polynomial &); //assignment (array) operator p1=p2   
+	const Polynomial &operator*(const Polynomial & );//p1*=p2
+	const Polynomial & operator+(const Polynomial &); // p1+k
+	//const Polynomial & operator+(const double)const; // p1-k
+	const Polynomial & operator-(const Polynomial &);// p1-p2
+	const double & operator[] (const int)const;
 	
+
 };
 
 #endif
