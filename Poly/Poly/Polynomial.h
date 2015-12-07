187088d480973e6a291de7a9ab8da7d471cdabe7
Melak <melakf2@gmail.com>
2015-12-07 12:35:54 +0200

    recode

--- Poly/Poly/Polynomial.h
+++ Poly/Poly/Polynomial.h
@@ -4,30 +4,14 @@
 using namespace std;
 class Polynomial
 {   	
-    //non member function
-	friend ostream & operator<<(ostream & output,const Polynomial &polynomial);
-	friend istream & operator>>(istream  & input, Polynomial &polynomial);
-	//non-member function, get two arguments, unlike non-static member function that get one argument.
-<<<<<<< HEAD
-	friend Polynomial & operator*(const double,const Polynomial &);
-	friend Polynomial &operator+(const double ,Polynomial&);
-	friend Polynomial &operator-(const double,Polynomial&);
-
-protected:
-=======
-	
-	//friend Polynomial &operator+(const double ,Polynomial&);
-	friend Polynomial &operator-(const double,Polynomial&);
-	//friend Polynomial &operator+(Polynomial & p1, const Polynomial & p2);
-	friend Polynomial &operator*(Polynomial &, const double);//p1*k
-	friend Polynomial &operator*(const double , Polynomial&);//k*p
 private:
->>>>>>> 424ebcf1cb2fde8b0218d1b9445341d557677abb
-	//data member
+	
+	//=======data member=======//
 	double * array_coefficient;
 	int size;
-public:
-	//member function
+
+public:	
+	//==============member function===================//	
 	Polynomial();
 	Polynomial(int); // ctor with degree zero & arr size is 1
 	~Polynomial(); // dctor
@@ -35,40 +19,28 @@ public:
 	void set_size_of_array(int); //setter 
 	int get_size_of_array()const; //getter
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 424ebcf1cb2fde8b0218d1b9445341d557677abb
-	//opertor-overloading
+	//==================opertor-overloading========================///
 	bool operator== (const Polynomial &) const;
 	bool operator!= (const Polynomial & right) const { return !(*this == right);};
 	bool operator> (const Polynomial &) const;
 	bool operator< (const Polynomial & right) const{ return !(*this > right);};
-	const Polynomial & operator+= (const Polynomial &); //operator+=
-<<<<<<< HEAD
-	const Polynomial & operator-= (const Polynomial & )const; // p1-=p2
-	const Polynomial &operator*=(const Polynomial & );//p1=p3*p2
-	const Polynomial & operator= (const Polynomial &); //assignment (array) operator p1=p2   
-	const Polynomial &operator*(const Polynomial & );//p1*=p2
-	const Polynomial &operator*(const double);// p1*=k
-	const Polynomial & operator+(const Polynomial &)const; // p1+k
-	const Polynomial & operator+(const double)const; // p1-k
-	const Polynomial & operator-(const Polynomial &);// p1+p2
-	const Polynomial & operator-(const Polynomial & )const; //p1-p2
-	int & operator[] (int);
-	const int & operator[] (int)const;
-=======
+	const Polynomial & operator+= (const Polynomial &); //p1+=p1
 	const Polynomial & operator-= (const Polynomial & ); // p1-=p2
-	const Polynomial &operator*=(const Polynomial & );//p1=p3*p2
+	const Polynomial &operator*=(const Polynomial & );//p1*=p2
 	const Polynomial & operator= (const Polynomial &); //assignment (array) operator p1=p2   
-	const Polynomial &operator*(const Polynomial & );//p1*=p2
-	const Polynomial & operator+(const Polynomial &); // p1+k
-	//const Polynomial & operator+(const double)const; // p1-k
+	const Polynomial &operator*(const Polynomial & );//p1*p2
+	const Polynomial & operator+(const Polynomial &); // p1+p2
 	const Polynomial & operator-(const Polynomial &);// p1-p2
-	const double & operator[] (const int)const;
-	
->>>>>>> 424ebcf1cb2fde8b0218d1b9445341d557677abb
+	const double & operator[] (const int)const; // return value by index
 
+	//===================non member function=================================//
+	friend ostream & operator<<(ostream & output,const Polynomial &polynomial);
+	friend istream & operator>>(istream  & input, Polynomial &polynomial);
+	
+	//==non-member function, get two arguments, unlike non-static member function that get one argument.==//
+	friend Polynomial &operator-(const double,Polynomial&);//p1-p2
+	friend Polynomial &operator*(Polynomial &, const double);//p1*k
+	friend Polynomial &operator*(const double , Polynomial&);//k*p
 };
 
 #endif
--- Poly/Poly/main.cpp
+++ Poly/Poly/main.cpp
@@ -1,31 +1,6 @@
 #include<iostream>
 #include "Polynomial.h"
 using namespace std;
-<<<<<<< HEAD
-int main()
-{
-	/*
-Polynomial p1( 1.0, -4.0 ); 
-Polynomial p2( 15.0, 1.0 ); 
-Polynomial p3 = p1 + p2;
-Polynomial p3 = p1 * p2;
-Polynomial p4(p1)
-p2 += p1; 
-p2 *= p1; 
-p2 = p3*p1; 
-p2 -= p1; 
-p3 = p1 + p2; 
-p2 = p1 + p4; 
-p3 = p1 * p3-p2-p1+p4; 
-p4 = p1 * p2*p3; 
-double k=5.0;
-p2=d*p1; 
-p2=p1*k;
-p2=d*p1;
-*/
-	return 0;
-}
-=======
 Polynomial &input_polynom(int);
 int get_size();
 int main()
@@ -202,4 +177,3 @@ int get_size()
 	}
 	return degree;
 }
->>>>>>> 424ebcf1cb2fde8b0218d1b9445341d557677abb
